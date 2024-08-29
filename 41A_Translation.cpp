64758393a6cccc3fb9671c75c6c9901451ea54fd
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2024-08-30 00:27:29 +0600

    41A_Translation

---
+++ 41A_Translation.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+
+    cin >> a >> b;
+
+    reverse(b.begin(), b.end());
+
+    if (a == b) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
+
