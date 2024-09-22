ad68974e92271993dcfc1f6b6049d5b511aca3cd
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2024-09-23 00:06:01 +0600

    1791A_Codeforces_Checking

---
+++ 1791A_Codeforces_Checking.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{    
+    int n;
+    char s;
+    cin>>n;
+
+    while(n--)
+    {   
+        cin>>s;
+
+        if(s == 'c')
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(s == 'o')
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(s == 'd')
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(s == 'e')
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(s == 'f')
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(s == 'r')
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(s == 's')
+        {
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+
+    return 0;
+
+}
+
+
+
