75dc4ba9b2a2a309ef006edfdd0f030d45047e77
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2024-11-29 01:09:57 +0600

    1399A_Remove_Smallest

---
+++ 1399A_Remove_Smallest.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int &x : a) {
+        cin >> x;
+    }
+
+    sort(a.begin(), a.end());
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] - a[i - 1] > 1) {
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
