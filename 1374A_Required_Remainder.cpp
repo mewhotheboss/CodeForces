ad0cdac31991d3563545da2d532389c904493b68
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-06-21 19:49:39 +0600

    1374A_Required_Remainder

---
+++ 1374A_Required_Remainder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long x, y, n;
+        cin >> x >> y >> n;
+
+        long long m = (n - y) / x;
+
+        long long k = m * x + y;
+
+        cout << k << "\n";
+    }
+
+    return 0;
+}
